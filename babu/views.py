07e6905e094af2f8d9cc308f24f99ad710c129f8
Leigh Brenecki <l@leigh.net.au>
2020-09-13 11:37:33 +0000

    basic working template decorator

---
+++ babu/views.py
@@ -0,0 +1,19 @@
+import jinja2
+import functools
+import typing as t
+from .response import Response
+
+environment = jinja2.Environment(
+	loader=jinja2.PackageLoader('test_app', 'templates'),
+	autoescape=jinja2.select_autoescape(('htm', 'html', 'xml'))
+)
+
+def template(name: str) -> t.Callable[[t.Callable], t.Callable]:
+	the_template = environment.get_template(name)
+	def decorator(func: t.Callable):
+		@functools.wraps(func)
+		def wrapper(*args, **kwargs):
+			context = func(*args, **kwargs)
+			return Response(200, the_template.render(**context))
+		return wrapper
+	return decorator
--- test_app/views.py
+++ test_app/views.py
@@ -1,10 +1,12 @@
 from babu.routing import Router, DatabaseRoute, query
 from . import models
 from babu.response import Response
+from babu.views import template
 
 app = Router()
 
 
 @app.db_view("/page/%s/", models.Page.id)
+@template('test.html')
 def foo(id):
-    return Response(status=200, body=f"ID: {id}")
+    return {'blah': id}
