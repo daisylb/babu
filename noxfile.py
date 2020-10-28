fbcabe2803e3ef99cb80baf705d3a1bbf2f0edda
Leigh Brenecki <l@leigh.net.au>
2020-10-28 21:54:22 +1030

    remove redundant nox task

--- noxfile.py
+++ noxfile.py
@@ -5,9 +5,3 @@ import nox
 def typecheck(session):
     session.install("mypy", "sqlalchemy-stubs")
     session.run("mypy", "babu")
-
-
-@nox.session
-def format(session):
-    session.install("black")
-    session.run("black", ".")
